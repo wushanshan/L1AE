633bfe80ad4689a70edb0c0e633151b8282924bc
Shanshan Wu <shanshan@utexas.edu>
2018-06-26 20:28:14 -0400

    Add files via upload

---
+++ simpleAE_scripts/amazon.sh
@@ -0,0 +1,6 @@
+python amazon_simpleAE.py --emb_dim 20
+python amazon_simpleAE.py --emb_dim 40
+python amazon_simpleAE.py --emb_dim 60
+python amazon_simpleAE.py --emb_dim 80
+python amazon_simpleAE.py --emb_dim 100
+python amazon_simpleAE.py --emb_dim 120
---
+++ simpleAE_scripts/synthetic1.sh
@@ -0,0 +1,7 @@
+python synthetic_simpleAE.py --block_dim 10 --sparsity_level 1 --emb_dim 10 --num_random_dataset 10
+python synthetic_simpleAE.py --block_dim 10 --sparsity_level 1 --emb_dim 20 --num_random_dataset 10
+python synthetic_simpleAE.py --block_dim 10 --sparsity_level 1 --emb_dim 30 --num_random_dataset 10
+python synthetic_simpleAE.py --block_dim 10 --sparsity_level 1 --emb_dim 40 --num_random_dataset 10
+python synthetic_simpleAE.py --block_dim 10 --sparsity_level 1 --emb_dim 50 --num_random_dataset 10
+python synthetic_simpleAE.py --block_dim 10 --sparsity_level 1 --emb_dim 60 --num_random_dataset 10
+python synthetic_simpleAE.py --block_dim 10 --sparsity_level 1 --emb_dim 70 --num_random_dataset 10
